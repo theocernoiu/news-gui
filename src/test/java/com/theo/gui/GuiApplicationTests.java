124e90c27197ab6fe88833f05050ddc4c4e0491f
tcernoiu <Theodor.Cernoiu.extern@ssi-schaefer.com>
2021-05-09 21:14:47 +0300

    Initial Commit

---
+++ src/test/java/com/theo/gui/GuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theo.gui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
